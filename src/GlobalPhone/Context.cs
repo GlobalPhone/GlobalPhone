3c671a8c36b7ee37e303317f26d0e2fd789dd3f0
Oskar Gewalli <oskar@gewalli.se>
2014-03-31 09:52:50 +0200

    Release

--- src/GlobalPhone/Context.cs
+++ src/GlobalPhone/Context.cs
@@ -69,6 +69,10 @@ namespace GlobalPhone
             {
                 return false;
             }
+            catch (UnknownTerritoryException)
+            {
+                return false;
+            }
         }
 
     }
