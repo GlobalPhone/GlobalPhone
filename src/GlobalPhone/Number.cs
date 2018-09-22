44ee23c7db87a7d8be693f3b783cb0c5b74fb716
Oskar Gewalli <oskar@gewalli.se>
2018-09-22 11:44:27 +0200

    Generate xml docs

--- src/GlobalPhone/Number.cs
+++ src/GlobalPhone/Number.cs
@@ -5,9 +5,18 @@ using PhoneNumbers;
 
 namespace GlobalPhone
 {
+    /// <summary>
+    /// A wrapper class around <see cref="PhoneNumbers.PhoneNumber"/> in order to provide additional functionality. It has a
+    /// reference to <see cref="PhoneNumberUtil"/> in order to be able to give a nicer API.
+    /// The downside is that this implies that there are more references per number. This might not matter
+    /// in many scenarios.
+    /// </summary>
     public class Number:IEquatable<Number>
     {
-        private readonly PhoneNumber _num;
+        /// <summary>
+        /// Get the wrapped phone number
+        /// </summary>
+        public readonly PhoneNumber PhoneNumber;
         private readonly PhoneNumberUtil _util;
 
         private static readonly Regex NonDialableChars = new Regex("[^,#+\\*\\d]", RegexOptions.Compiled);
@@ -15,13 +24,13 @@ namespace GlobalPhone
         private string _internationalFormat;
         private string _internationalString;
 
-        internal Number(PhoneNumber num, PhoneNumberUtil util)
+        internal Number(PhoneNumber phoneNumber, PhoneNumberUtil util)
         {
-            _num = num;
+            PhoneNumber = phoneNumber;
             _util = util;
         }
 
-        public ulong NationalNumber => _num.NationalNumber;
+        public ulong NationalNumber => PhoneNumber.NationalNumber;
         /// <summary>
         /// Gets the area code. For instance the "702" part of "+1 702-389-1234".
         /// </summary>
@@ -30,8 +39,8 @@ namespace GlobalPhone
         {
             get
             {
-                var areaCodeLength = _util.GetLengthOfGeographicalAreaCode(_num);
-                var region = _util.GetRegionCodeForNumber(_num);
+                var areaCodeLength = _util.GetLengthOfGeographicalAreaCode(PhoneNumber);
+                var region = _util.GetRegionCodeForNumber(PhoneNumber);
                 var metadata = _util.GetMetadataForRegion(region);
                 
                 if (areaCodeLength > 0)
@@ -42,7 +51,7 @@ namespace GlobalPhone
                         values.Add(metadata.NationalPrefix);
                     }
                     
-                    values.Add(_util.GetNationalSignificantNumber(_num).Substring(0, areaCodeLength));
+                    values.Add(_util.GetNationalSignificantNumber(PhoneNumber).Substring(0, areaCodeLength));
                     return string.Concat(values);
                 }
                 return "";
@@ -58,8 +67,8 @@ namespace GlobalPhone
         {
             get
             {
-                var areaCodeLength = _util.GetLengthOfGeographicalAreaCode(_num);
-                var nationalSignificantNumber = _util.GetNationalSignificantNumber(_num);
+                var areaCodeLength = _util.GetLengthOfGeographicalAreaCode(PhoneNumber);
+                var nationalSignificantNumber = _util.GetNationalSignificantNumber(PhoneNumber);
                 
                 return areaCodeLength > 0
                     ? nationalSignificantNumber.Substring(areaCodeLength)
@@ -68,10 +77,23 @@ namespace GlobalPhone
         }
 
         /// <summary>
+        /// Metadata for number
+        /// </summary>
+        public PhoneMetadata PhoneMetadata =>
+            _util.GetMetadataForRegion(_util.GetRegionCodeForNumber(PhoneNumber));
+
+        /// <summary>
+        /// Region code for number
+        /// </summary>
+        public string RegionCode =>
+            _util.GetRegionCodeForNumber(PhoneNumber);
+
+
+        /// <summary>
         /// For instance "0771-79 33 36" of "+46 771 793 336"
         /// or "(650) 253-0000" of "+1 650-253-0000"
         /// </summary>
-        public string FormattedNationalString => _util.Format(_num, PhoneNumberFormat.NATIONAL);
+        public string FormattedNationalString => _util.Format(PhoneNumber, PhoneNumberFormat.NATIONAL);
 
         /// <summary>
         /// For instance (312) 555-1212 for the number 312-555-1212 (us)
@@ -79,23 +101,23 @@ namespace GlobalPhone
         /// </summary>
         /// <value>The national format.</value>
         public string NationalFormat =>
-            _nationalFormat ?? (_nationalFormat = _util.Format(_num, PhoneNumberFormat.NATIONAL));
+            _nationalFormat ?? (_nationalFormat = _util.Format(PhoneNumber, PhoneNumberFormat.NATIONAL));
         /// <summary>
         /// Gets the international format. For instance "+1 312-555-1212" or "+46 771 79 33 36".
         /// </summary>
         /// <value>The international format.</value>
         public string InternationalFormat => _internationalFormat ??
                                              (_internationalFormat =
-                                                 _util.Format(_num, PhoneNumberFormat.INTERNATIONAL));
+                                                 _util.Format(PhoneNumber, PhoneNumberFormat.INTERNATIONAL));
 
-        public int CountryCode => _num.CountryCode;
+        public int CountryCode => PhoneNumber.CountryCode;
 
-        public bool IsValid => _util.IsValidNumber(_num);
+        public bool IsValid => _util.IsValidNumber(PhoneNumber);
         /// <summary>
         /// For instance 3125551212 of "(312) 555-1212" or 0771793336 of "+46 771 793 336"
         /// </summary>
         public string NationalString =>
-            _util.Format(_num, PhoneNumberFormat.NATIONAL)
+            _util.Format(PhoneNumber, PhoneNumberFormat.NATIONAL)
                 .Yield(s => NonDialableChars.Replace(s, ""));
 
         /// <summary>
@@ -106,9 +128,9 @@ namespace GlobalPhone
         
         public override bool Equals(object obj) => Equals(obj as Number);
 
-        public bool Equals(Number obj) => !ReferenceEquals(null, obj) && _num.Equals(obj._num);
+        public bool Equals(Number obj) => !ReferenceEquals(null, obj) && PhoneNumber.Equals(obj.PhoneNumber);
 
-        public override int GetHashCode() => _num.GetHashCode();
+        public override int GetHashCode() => PhoneNumber.GetHashCode();
 
         public override string ToString() => InternationalString;
 
